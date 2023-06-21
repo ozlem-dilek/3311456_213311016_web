038e393888f3de603f6127e6e6d7cfb24c5349f1
Özlem Dilek Acar <acarozlemdilek@gmail.com>
2023-06-21 12:08:55 +0300

    commit deneme

---
+++ index.dart
@@ -0,0 +1,26 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:studyapp2/views/welcome.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  runApp(const StudyApp());
+}
+
+class StudyApp extends StatelessWidget {
+  const StudyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Study App',
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+      ),
+      home: welcome(),
+    );
+  }
+
+}
